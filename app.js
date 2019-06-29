fa0f7df57b727c5f415c329510ad3bf97933e053
pooja <pooja@coursera.com>
2019-06-30 00:19:18 +0530

    New Assignment

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-var student = {
-  name: "",
-  type: "student"
+(function () {
+'use strict';
+
+angular.module('DIApp',[])
+
+.controller('DIController',DIController);
+function DIController ($scope,$filter){
+  $scope.name="Poo";
+  $scope.upper =function(){
+    var uppCase = $filter('uppercase');
+    $scope.name = uppCase($scope.name);
+  };
 }
-document.addEventListener('DomContentLoaded',contentLoaded);
 
-function contentLoaded(event){
-  document.getElementById('name').addEventListener("keyup",keyUp);
-}
-
-function keyUp(event){
-  calculateNumericOutput();
-}
-
-function calculateNumericOutput(){
-  student.name = document.getElementById('name').value;
-
-  var totalNameValue = 0;
-  for(var i=0;i<student.name.length;i++){
-    totalNameValue+= student.name.charCodeAt(i);
-  }
-
-  //insert result into page
-
-  var output = "Total Numeric value of a person's name is: " + totalNameValue;
-  document.getElementById('output').innerText = output;
-}
+})();
